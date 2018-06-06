14e68f982e63b69abdc4c1579e3df5dfa15b260b
Jakub Lamprecht <jakub.lamprecht@gmail.com>
2018-06-06 18:14:17 +0200

    Changes to timeStamp. Added recording to launch

--- src/phone.cpp
+++ src/phone.cpp
@@ -13,22 +13,18 @@ int main(int argc, char **argv)
 {
   int startSec;
   int startNsec;
-  ros::Duration timeDiff;
-  ros::Time previousTime;
-  ros::Time timeNow;
   ros::Time timeStamp;
+  ros::Duration timeStampDiff;
   string line = "";
   string filePath = "";
   fstream dataFile;
+  long prevQt = 0;
   
   ros::init(argc, argv, "phone_publisher");
   
   ros::NodeHandle n;
   ros::Publisher phonePub = n.advertise<sensor_msgs::Imu>("/phone/imu", 1000);
   ros::Rate loop_rate(100);
-
-  previousTime.sec = 0;
-  previousTime.nsec = 0;
   
   n.param<std::string>("/phone_publisher/dataFilePath", filePath, "dummypath.txt");
   n.getParam("/phone_publisher/secs", startSec);
@@ -46,25 +42,31 @@ int main(int argc, char **argv)
         break;
       }
       
-      float qw, qx, qy, qz, qt;
-      float ax, ay, az, at;
-      float gx, gy, gz, gt;
-      float mx, my, mz, mt;
+      float qw, qx, qy, qz;
+      float ax, ay, az;
+      float gx, gy, gz;
+      float mx, my, mz;
+      long qt, at, gt, mt;
 
       char tmp;
       dataFile >> tmp >> qw >> tmp >> qx >> tmp >> qy >> tmp >> qz >> tmp >> qt >> tmp;
       dataFile >> tmp >> ax >> tmp >> ay >> tmp >> az >> tmp >> at >> tmp;
       dataFile >> tmp >> gx >> tmp >> gy >> tmp >> gz >> tmp >> gt >> tmp;
       dataFile >> tmp >> mx >> tmp >> my >> tmp >> mz >> tmp >> mt >> tmp;
+
+      if(prevQt == 0) {
+        prevQt = qt;
+      }
           
-      ROS_INFO("Values Q: %f %f %f %f %f", qw, qx, qy, qz, qt);
-      ROS_INFO("Values A: %f %f %f %f", ax, ay, az, at);
-      ROS_INFO("Values G: %f %f %f %f", gx, gy, gz, gt);
-      ROS_INFO("Values M: %f %f %f %f", mx, my, mz, mt);
-      
-      timeNow = ros::Time::now();
-      timeDiff = timeNow - previousTime;
-      timeStamp = timeStamp + timeDiff;
+      ROS_INFO("Values Q: %f %f %f %f %ld", qw, qx, qy, qz, qt);
+      ROS_INFO("Values A: %f %f %f %ld", ax, ay, az, at);
+      ROS_INFO("Values G: %f %f %f %ld", gx, gy, gz, gt);
+      ROS_INFO("Values M: %f %f %f %ld", mx, my, mz, mt);
+
+      timeStampDiff.nsec = qt - prevQt;
+      ROS_INFO("Time diff: %ld", qt - prevQt);
+      prevQt = qt;
+      timeStamp = timeStamp + timeStampDiff;
 
       sensor_msgs::Imu imuMsg;
 
@@ -76,8 +78,6 @@ int main(int argc, char **argv)
       imuMsg.orientation.y = qy;
       imuMsg.orientation.z = qz;
 
-      // No data from phone
-
       imuMsg.angular_velocity.x = gx;
       imuMsg.angular_velocity.y = gy;
       imuMsg.angular_velocity.z = gz;
@@ -88,7 +88,6 @@ int main(int argc, char **argv)
 
       phonePub.publish(imuMsg);
 
-      previousTime = timeNow;
       ros::spinOnce();
       loop_rate.sleep();
     }
